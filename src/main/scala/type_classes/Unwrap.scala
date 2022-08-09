7e8197a652a49eecb38575b9f231a887262f030e
StefanosTouf <stefetoufe@gmail.com>
2022-08-09 12:05:44 +0300

    Add wrap function to Unwrap type class

--- src/main/scala/type_classes/Unwrap.scala
+++ src/main/scala/type_classes/Unwrap.scala
@@ -2,8 +2,14 @@ package type_classes
 
 trait Unwrap[A, T]:
   def unwrap(a: A): T
+  def wrap(t: T): A
 
 object Unwrap:
   def apply[A, T](using Unwrap[A, T]): Unwrap[A, T] = summon
+
+  def make[A, T](f1: A => T, f2: T => A): Unwrap[A, T] =
+    new:
+      def unwrap(a: A) = f1(a)
+      def wrap(t: T)   = f2(t)
 
   extension [A](a: A) def unwrap[T](using Unwrap[A, T]): T = Unwrap[A, T].unwrap(a)
